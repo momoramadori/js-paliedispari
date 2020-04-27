6fb4343b61bda27b72af5e91bf09973aab675345
Momo Ramadori <momoramadori8@gmail.com>
2020-04-27 10:32:46 +0200

    sistemato es 1

--- palindroma/main.js
+++ palindroma/main.js
@@ -5,20 +5,25 @@ var utentValue = prompt('La parola che inserisci è palindroma?');
 console.log(utentValue);
 
 // Creo una funzione che mi permetta di capire se la parola è palindroma
-function palindrome(parola) {
+function invertWord(parola) {
     var wordToArray = parola.split('');
     var invertArray = wordToArray.reverse();
     var arrayToInvWord = invertArray.join('');
     return arrayToInvWord;
-    console.log(arrayToInvWord);
 }
-
-var parola_palindroma = palindrome(utentValue);
-
-if (parola_palindroma == utentValue) {
-    //Se palindrome() è true allora la parola è palindroma
+//Creo una funzione che verifichi se la parola è palindroma o meno
+function parolaPalindroma(parola) {
+    if (invertWord(parola) == parola) {
+        //Se palindrome() è true allora la parola è palindroma
+        return true;
+    } else {
+        //Se palidnrome() è false allora la parola non è palindroma
+        return false
+    }
+}
+//Creo un if per stampare il contenuto adatto in base all'esito della funzione
+if (parolaPalindroma(utentValue)) {
     console.log('la parola è palindroma');
 } else {
-    //Se palidnrome() è false allora la parola non è palindroma
     console.log('la parola non è palindroma');
 }
